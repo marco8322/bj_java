b3db40370c97c23018614ee931d0b5b4279c12a5
Marco Jacques <marco.jacques@gmail.com>
2015-03-24 20:04:32 -0400

    Finish BJGame unit tests

--- test/org/mj/blackjack/game/TestBJGame.java
+++ test/org/mj/blackjack/game/TestBJGame.java
@@ -96,4 +96,255 @@ public class TestBJGame
         assertEquals(-100, players.get(0).getMoneyAmount());
         assertEquals(0, players.get(1).getMoneyAmount());
     }
+
+    /**
+     * Test with hit/stay
+     */
+    public void testHit()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(false, 3, false),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(1).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(2).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(3).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(10), new BJCardImpl(10), new BJCardImpl(6), new BJCardImpl(5), new BJCardImpl(10),
+                        new BJCardImpl(4), new BJCardImpl(5), new BJCardImpl(7), new BJCardImpl(4), new BJCardImpl(9),
+                        new BJCardImpl(8), new BJCardImpl(5), new BJCardImpl(6), new BJCardImpl(4)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(
+                        BJMove.HIT,
+                        BJMove.HIT, BJMove.STAY,
+                        BJMove.HIT, BJMove.STAY,
+                        BJMove.HIT, BJMove.STAY
+                        )
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(-100, players.get(0).getMoneyAmount());
+        assertEquals(100, players.get(1).getMoneyAmount());
+        assertEquals(0, players.get(2).getMoneyAmount());
+        assertEquals(-100, players.get(3).getMoneyAmount());
+    }
+
+    /**
+     * Test with doubles
+     */
+    public void testDouble()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(false, 3, false),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(1).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(2).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(3).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(2), new BJCardImpl(9), new BJCardImpl(10), new BJCardImpl(9), new BJCardImpl(10),
+                        new BJCardImpl(9), new BJCardImpl(3), new BJCardImpl(5), new BJCardImpl(4), new BJCardImpl(6),
+                        new BJCardImpl(6), new BJCardImpl(8), new BJCardImpl(8), new BJCardImpl(5), new BJCardImpl(2)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(BJMove.DOUBLE, BJMove.DOUBLE, BJMove.DOUBLE, BJMove.DOUBLE)
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(-200, players.get(0).getMoneyAmount());
+        assertEquals(200, players.get(1).getMoneyAmount());
+        assertEquals(-200, players.get(2).getMoneyAmount());
+        assertEquals(0, players.get(3).getMoneyAmount());
+    }
+
+    /**
+     * Test with split
+     */
+    public void testSplit()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(false, 1, false),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(1).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(2).setInitialBet(100);
+        players.add(new BJPlayerImpl(0));
+        players.get(3).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(2), new BJCardImpl(9), new BJCardImpl(5), new BJCardImpl(4), new BJCardImpl(10),
+                        new BJCardImpl(2), new BJCardImpl(9), new BJCardImpl(5), new BJCardImpl(4), new BJCardImpl(6),
+                        new BJCardImpl(6), new BJCardImpl(10), new BJCardImpl(7), new BJCardImpl(11),
+                        new BJCardImpl(8), new BJCardImpl(7),
+                        new BJCardImpl(10), new BJCardImpl(4), new BJCardImpl(4), new BJCardImpl(10),
+                        new BJCardImpl(5), new BJCardImpl(6), new BJCardImpl(6), new BJCardImpl(6),
+                        new BJCardImpl(2)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(
+                        BJMove.SPLIT, BJMove.DOUBLE, BJMove.DOUBLE,
+                        BJMove.SPLIT, BJMove.STAY, BJMove.STAY,
+                        BJMove.SPLIT, BJMove.HIT, BJMove.STAY, BJMove.HIT, BJMove.STAY,
+                        BJMove.SPLIT, BJMove.HIT, BJMove.STAY, BJMove.DOUBLE
+                )
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(200, players.get(0).getMoneyAmount());
+        assertEquals(-200, players.get(1).getMoneyAmount());
+        assertEquals(200, players.get(2).getMoneyAmount());
+        assertEquals(-300, players.get(3).getMoneyAmount());
+    }
+
+    /**
+     * Player surrender
+     */
+    public void testSurrender()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(false, 3, true),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(10), new BJCardImpl(4),
+                        new BJCardImpl(6), new BJCardImpl(8)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(BJMove.SURRENDER)
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(-50, players.get(0).getMoneyAmount());
+    }
+
+
+    /**
+     * Test dealer hit soft 17
+     */
+    public void testDealerHitSoft17()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(true, 3, false),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(10), new BJCardImpl(6),
+                        new BJCardImpl(8), new BJCardImpl(11),
+                        new BJCardImpl(2)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(BJMove.STAY)
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(-100, players.get(0).getMoneyAmount());
+    }
+
+
+    /**
+     * Test dealer stay soft 17
+     */
+    public void testDealerStaySoft17()
+    {
+        BJGame game = new BJGame(
+                new BJFactoryImpl(),
+                new BJSettingsImpl(
+                        new BJStandardPossibleMoves(),
+                        new BJStandardRules(false, 3, false),
+                        new BJStandardPayout()
+                )
+        );
+
+        List<BJPlayer> players = new LinkedList<BJPlayer>();
+        players.add(new BJPlayerImpl(0));
+        players.get(0).setInitialBet(100);
+
+        BJCardDeck cardDeck = new BJCardDeckImpl(
+                new BJCard[]{
+                        new BJCardImpl(10), new BJCardImpl(6),
+                        new BJCardImpl(8), new BJCardImpl(11)
+                }
+        );
+
+        BJNextMove nextMove = TestGameUtil.createBJNextMove(
+                Arrays.asList(BJMove.STAY)
+        );
+
+        game.playGame(players, cardDeck, nextMove);
+
+        assertEquals(100, players.get(0).getMoneyAmount());
+    }
 }
