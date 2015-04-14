c5f3c53d627e210fcaaf2e440d72a98b8e899b5f
Marco Jacques <marco.jacques@gmail.com>
2015-04-13 21:27:51 -0400

    Add command-line parsing

--- src/org/mj/blackjack/main/BJCompareMain.java
+++ src/org/mj/blackjack/main/BJCompareMain.java
@@ -2,13 +2,14 @@ package org.mj.blackjack.main;
 
 import org.mj.blackjack.card.BJCardDeck;
 import org.mj.blackjack.card.BJKnownFirstCardsCardDeck;
-import org.mj.blackjack.card.BJMultipleCardDecks;
 import org.mj.blackjack.factory.BJFactory;
 import org.mj.blackjack.factory.BJFactoryImpl;
 import org.mj.blackjack.game.BJGame;
 import org.mj.blackjack.game.BJSettings;
 import org.mj.blackjack.game.BJSettingsImpl;
-import org.mj.blackjack.moves.*;
+import org.mj.blackjack.moves.BJCompareNextMove;
+import org.mj.blackjack.moves.BJMove;
+import org.mj.blackjack.moves.BJStandardPossibleMoves;
 import org.mj.blackjack.payout.BJStandardPayout;
 import org.mj.blackjack.player.BJPlayer;
 import org.mj.blackjack.player.BJPlayerImpl;
@@ -24,12 +25,41 @@ import java.util.Arrays;
  */
 public class BJCompareMain
 {
+    private static class Args
+    {
+        private int playerHand;
+        private BJKnownFirstCardsCardDeck.Kind kind;
+        private BJMove nextMove;
+        private int dealerCard;
+        private int numberDecks;
+
+        Args(int playerHand_,
+             BJKnownFirstCardsCardDeck.Kind kind_,
+             BJMove nextMove_,
+             int dealerCard_,
+             int numberDecks_
+        )
+        {
+            playerHand = playerHand_;
+            kind = kind_;
+            nextMove = nextMove_;
+            dealerCard = dealerCard_;
+            numberDecks = numberDecks_;
+        }
+    }
+
     public static void main(String[] args)
     {
         final int MAX_LOOPS = 100000;
 
-        BJCompareNextMove nextMove = new BJCompareNextMove(BJMove.DOUBLE);
-        BJCardDeck cardDeck = new BJKnownFirstCardsCardDeck(18, BJKnownFirstCardsCardDeck.Kind.Soft, 2, 6);
+        Args parsedArgs = parseCommandLine(args);
+
+        BJCompareNextMove nextMove = new BJCompareNextMove(parsedArgs.nextMove);
+        BJCardDeck cardDeck = new BJKnownFirstCardsCardDeck(
+                parsedArgs.playerHand, parsedArgs.kind,
+                parsedArgs.dealerCard, parsedArgs.numberDecks
+        );
+
         BJSettings settings = new BJSettingsImpl(
                 new BJStandardPossibleMoves(),
                 new BJStandardRules(false, 3, false),
@@ -61,5 +91,66 @@ public class BJCompareMain
 
         double average = (((double)totalAmount) / (double)MAX_LOOPS);
         System.out.println(new DecimalFormat("#0.00").format(average));
+    }
+
+    private static Args parseCommandLine(String[] args)
+    {
+        int playerHand = 18;
+        BJKnownFirstCardsCardDeck.Kind kind = BJKnownFirstCardsCardDeck.Kind.Normal;
+        BJMove nextMove = BJMove.STAY;
+        int dealerCard = 10;
+        int numberDecks = 6;
+
+        for( String arg: args )
+        {
+            String[] nameValue = arg.split(":");
+            if( nameValue.length != 2 )
+            {
+                System.out.println("Invalid argument syntax");
+                System.exit(1);
+            }
+
+            String name = nameValue[0];
+            String value = nameValue[1];
+
+            if( name.equals("playerHand") )
+            {
+                playerHand = Integer.parseInt(value);
+            }
+            else if( name.equals("kind") )
+            {
+                kind = BJKnownFirstCardsCardDeck.Kind.valueOf(value);
+                if( kind == null )
+                {
+                    System.out.println("Invalid kind: " + value);
+                    System.exit(1);
+                }
+            }
+            else if( name.equals("nextMove") )
+            {
+                nextMove = BJMove.valueOf(value);
+
+                if( nextMove == null )
+                {
+                    System.out.println("Invalid next value: " + value);
+                    System.exit(1);
+                }
+            }
+            else if( name.equals("dealerCard") )
+            {
+                dealerCard = Integer.parseInt(value);
+            }
+            else if( name.equals("numberDecks") )
+            {
+                numberDecks = Integer.parseInt(value);
+            }
+            else
+            {
+                System.out.println("Invalid argument name: " + name);
+                System.exit(1);
+            }
+        }
+
+        return new Args(playerHand, kind, nextMove, dealerCard, numberDecks);
     }
 }
